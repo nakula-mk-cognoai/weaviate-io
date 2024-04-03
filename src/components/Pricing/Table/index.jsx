9a8e2ed257f163e6d477559198d9a3ebba7c37e7
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-04-03 12:14:33 +0100

    update

--- src/components/Pricing/Table/index.jsx
+++ src/components/Pricing/Table/index.jsx
@@ -59,215 +59,221 @@ export default function PricingTable() {
                     <span className={styles.cellSpan}>Pricing type</span>
                   </div>
                 </div>
-                {/* Serverless Table */}
-                <div className={styles.serverlessTable}>
-                  <div className={styles.longCell}>
-                    <div className={styles.cellContent}>
-                      <div className={styles.cellImage}></div>
-                      <h3>Serverless</h3>
-                      <span>
-                        We manage everything for you in Weaviate Cloud Services
-                        (WCS)
+                {/* Table Content */}
+                <div className={styles.tableContent}>
+                  {/* Serverless Table */}
+                  <div className={styles.serverlessTable}>
+                    <div className={styles.longCell}>
+                      <div className={styles.cellContent}>
+                        <div className={styles.cellImage}></div>
+                        <h3>Serverless</h3>
+                        <span>
+                          We manage everything for you in Weaviate Cloud
+                          Services (WCS)
+                        </span>
+                      </div>
+                    </div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <span className={styles.cellSpan}>
+                        Building and prototyping with seamless scaling and
+                        flexible pay-as-you-go pricing
                       </span>
                     </div>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <span className={styles.cellSpan}>
-                      Building and prototyping with seamless scaling and
-                      flexible pay-as-you-go pricing
-                    </span>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <div className={styles.cellContent}>
-                      <div className={styles.cellLogo}>
-                        <div className={styles.logoText}>
-                          <span>Weaviate</span>
-                          <span>Cloud Services</span>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <div className={styles.cellContent}>
+                        <div className={styles.cellLogo}>
+                          <div className={styles.logoText}>
+                            <span>Weaviate</span>
+                            <span>Cloud Services</span>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <hr></hr>
-                  <div className={`${styles.longCell} ${styles.pricing}`}>
-                    <span className={styles.cellSpan}>
-                      Charges are consumption-based, pay-as-you-go, determined
-                      by the dimensions stored and the chosen SLA package
-                    </span>
-                    <div className={styles.buttons}>
-                      <Link
-                        className={styles.buttonGradient}
-                        onClick={openModal}
-                      >
-                        Sign up
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className={`${styles.modals} ${
-                    isModalOpen ? styles.open : ''
-                  }`}
-                  style={{ display: isModalOpen ? 'flex' : 'none' }}
-                >
-                  <div className={styles.modalContents}>
-                    <span className={styles.close} onClick={closeModal}>
-                      &times;
-                    </span>
-                    <SlaPlan />
-                  </div>
-                </div>
-
-                {/* Enterprise Table */}
-                <div
-                  className={`${styles.serverlessTable} ${styles.enterprise}`}
-                >
-                  <div className={styles.longCell}>
-                    <div className={styles.cellContent}>
-                      <div
-                        className={`${styles.edImage} ${styles.cellImage} `}
-                      ></div>
-                      <h3>Enterprise Dedicated</h3>
-                      <span>
-                        We manage everything for you in a dedicated instance in
-                        Weaviate Cloud Services (WCS)
+                    <hr></hr>
+                    <div className={`${styles.longCell} ${styles.pricing}`}>
+                      <span className={styles.cellSpan}>
+                        Charges are consumption-based, pay-as-you-go, determined
+                        by the dimensions stored and the chosen SLA package
                       </span>
-                    </div>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <span className={styles.cellSpan}>
-                      Deploying large-scale production use cases without the
-                      complexities of self-management
-                    </span>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <div className={styles.cellContent}>
-                      <div className={styles.iconSection}>
-                        <div
-                          className={`${styles.cellIcon} ${styles.azureIcon}`}
-                        ></div>
-                        <div
-                          className={`${styles.cellIcon} ${styles.awsIcon}`}
-                        ></div>
-                        <div
-                          className={`${styles.cellIcon} ${styles.googleIcon}`}
-                        ></div>
+                      <div className={styles.buttons}>
+                        <Link
+                          className={styles.buttonGradient}
+                          onClick={openModal}
+                        >
+                          Sign up
+                        </Link>
                       </div>
                     </div>
                   </div>
-                  <hr></hr>
-                  <div className={`${styles.longCell} ${styles.pricing}`}>
-                    <span className={styles.cellSpan}>
-                      Annual contract based on reserved resources and expected
-                      usage patterns
-                    </span>
-                    <div className={styles.buttons}>
-                      <Link
-                        to="#contact-sales"
-                        className={styles.buttonGradient}
-                      >
-                        Contact us
-                      </Link>
+                  <div
+                    className={`${styles.modals} ${
+                      isModalOpen ? styles.open : ''
+                    }`}
+                    style={{ display: isModalOpen ? 'flex' : 'none' }}
+                  >
+                    <div className={styles.modalContents}>
+                      <span className={styles.close} onClick={closeModal}>
+                        &times;
+                      </span>
+                      <SlaPlan />
                     </div>
                   </div>
-                </div>
-                {/* BYOC Table */}
-                <div className={`${styles.serverlessTable} ${styles.byoc}`}>
-                  <div className={styles.longCell}>
-                    <div className={styles.cellContent}>
-                      <div
-                        className={`${styles.byocImage} ${styles.cellImage} `}
-                      ></div>
-                      <h3>Bring Your Own Cloud</h3>
-                      <span>A fully managed solution within your VPC</span>
+
+                  {/* Enterprise Table */}
+                  <div
+                    className={`${styles.serverlessTable} ${styles.enterprise}`}
+                  >
+                    <div className={styles.longCell}>
+                      <div className={styles.cellContent}>
+                        <div
+                          className={`${styles.edImage} ${styles.cellImage} `}
+                        ></div>
+                        <h3>Enterprise Dedicated</h3>
+                        <span>
+                          We manage everything for you in a dedicated instance
+                          in Weaviate Cloud Services (WCS)
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <span className={styles.cellSpan}>
-                      Running workflows within your Virtual Private Cloud (VPC)
-                    </span>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <div className={styles.cellContent}>
-                      <div className={styles.iconSection}>
-                        <div
-                          className={`${styles.cellIcon} ${styles.azureIcon}`}
-                        ></div>
-                        <div
-                          className={`${styles.cellIcon} ${styles.awsIcon}`}
-                        ></div>
-                        <div
-                          className={`${styles.cellIcon} ${styles.googleIcon}`}
-                        ></div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <span className={styles.cellSpan}>
+                        Deploying large-scale production use cases without the
+                        complexities of self-management
+                      </span>
+                    </div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <div className={styles.cellContent}>
+                        <div className={styles.iconSection}>
+                          <div
+                            className={`${styles.cellIcon} ${styles.azureIcon}`}
+                          ></div>
+                          <div
+                            className={`${styles.cellIcon} ${styles.awsIcon}`}
+                          ></div>
+                          <div
+                            className={`${styles.cellIcon} ${styles.googleIcon}`}
+                          ></div>
+                        </div>
+                      </div>
+                    </div>
+                    <hr></hr>
+                    <div className={`${styles.longCell} ${styles.pricing}`}>
+                      <span className={styles.cellSpan}>
+                        Annual contract based on reserved resources and expected
+                        usage patterns
+                      </span>
+                      <div className={styles.buttons}>
+                        <Link
+                          to="#contact-sales"
+                          className={styles.buttonGradient}
+                        >
+                          Contact us
+                        </Link>
                       </div>
                     </div>
                   </div>
-                  <hr></hr>
-                  <div className={`${styles.longCell} ${styles.pricing}`}>
-                    <span className={styles.cellSpan}>
-                      Annual contract based upon CPU + RAM utilization
-                    </span>
-                    <div className={styles.buttons}>
-                      <Link
-                        to="#contact-sales"
-                        className={styles.buttonGradient}
-                      >
-                        Contact us
-                      </Link>
+                  {/* BYOC Table */}
+                  <div className={`${styles.serverlessTable} ${styles.byoc}`}>
+                    <div className={styles.longCell}>
+                      <div className={styles.cellContent}>
+                        <div
+                          className={`${styles.byocImage} ${styles.cellImage} `}
+                        ></div>
+                        <h3>Bring Your Own Cloud</h3>
+                        <span>A fully managed solution within your VPC</span>
+                      </div>
+                    </div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <span className={styles.cellSpan}>
+                        Running workflows within your Virtual Private Cloud
+                        (VPC)
+                      </span>
+                    </div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <div className={styles.cellContent}>
+                        <div className={styles.iconSection}>
+                          <div
+                            className={`${styles.cellIcon} ${styles.azureIcon}`}
+                          ></div>
+                          <div
+                            className={`${styles.cellIcon} ${styles.awsIcon}`}
+                          ></div>
+                          <div
+                            className={`${styles.cellIcon} ${styles.googleIcon}`}
+                          ></div>
+                        </div>
+                      </div>
+                    </div>
+                    <hr></hr>
+                    <div className={`${styles.longCell} ${styles.pricing}`}>
+                      <span className={styles.cellSpan}>
+                        Annual contract based upon CPU + RAM utilization
+                      </span>
+                      <div className={styles.buttons}>
+                        <Link
+                          to="#contact-sales"
+                          className={styles.buttonGradient}
+                        >
+                          Contact us
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                {/* Education & Support Table */}
-                <div className={`${styles.serverlessTable} ${styles.support}`}>
-                  <div className={styles.longCell}>
-                    <div className={styles.cellContent}>
-                      <div
-                        className={`${styles.byocImage} ${styles.cellImage} `}
-                      ></div>
-                      <h3>Education & Training</h3>
-                      <span>
-                        A variety of bespoke and self-service support and
-                        training options to accelerate adoption and success
+                  {/* Education & Support Table */}
+                  <div
+                    className={`${styles.serverlessTable} ${styles.support}`}
+                  >
+                    <div className={styles.longCell}>
+                      <div className={styles.cellContent}>
+                        <div
+                          className={`${styles.byocImage} ${styles.cellImage} `}
+                        ></div>
+                        <h3>Education & Training</h3>
+                        <span>
+                          A variety of bespoke and self-service support and
+                          training options to accelerate adoption and success
+                        </span>
+                      </div>
+                    </div>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <span className={styles.cellSpan}>
+                        Everyone! Anyone looking for help building and scaling
+                        with Weaviate
                       </span>
                     </div>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <span className={styles.cellSpan}>
-                      Everyone! Anyone looking for help building and scaling
-                      with Weaviate
-                    </span>
-                  </div>
-                  <hr></hr>
-                  <div className={styles.smallCell}>
-                    <div className={styles.cellContent}>
-                      <span>
-                        Support for any type of user on any type of deployment
-                      </span>
-                      <br></br>
-                      <span className={styles.cellNotice}>
-                        *Self-serve with on-demand resources
-                      </span>
+                    <hr></hr>
+                    <div className={styles.smallCell}>
+                      <div className={styles.cellContent}>
+                        <span>
+                          Support for any type of user on any type of deployment
+                        </span>
+                        <br></br>
+                        <span className={styles.cellNotice}>
+                          *Self-serve with on-demand resources
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                  <hr></hr>
-                  <div className={`${styles.longCell} ${styles.pricing}`}>
-                    <span className={styles.cellSpan}>
-                      Annual contract based upon CPU + RAM utilization
-                    </span>
-                    <div className={styles.buttons}>
-                      <Link
-                        to="#contact-sales"
-                        className={styles.buttonGradient}
-                      >
-                        Contact us
-                      </Link>
+                    <hr></hr>
+                    <div className={`${styles.longCell} ${styles.pricing}`}>
+                      <span className={styles.cellSpan}>
+                        Annual contract based upon CPU + RAM utilization
+                      </span>
+                      <div className={styles.buttons}>
+                        <Link
+                          to="#contact-sales"
+                          className={styles.buttonGradient}
+                        >
+                          Contact us
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
