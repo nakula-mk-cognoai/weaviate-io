25d38c0dc841763cd19c0d3a2d7b79ec859226bb
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-04-24 14:44:42 +0100

    Content Update

--- src/components/Events/OnDemand/index.jsx
+++ src/components/Events/OnDemand/index.jsx
@@ -9,29 +9,34 @@ export default function OnDemand() {
         <div className={styles.marketTitle}>
           <h2 className={styles.marketText}>On-demand webinars</h2>
         </div>
-        <div className={styles.buttons}>
+        <div className={styles.ondemandSection}>
           <div className={styles.marketBox}>
-            <p>
-              Intro to Hybrid Search: Combining the power of keyword and vector
-              search
-            </p>
-            <Link to="https://events.weaviate.io/hybrid-search-webinar">
-              {'Register now >'}
+            <div className={styles.marketImage}></div>
+            <span>WEBINAR</span>
+            <p>How to build an AI-Native foundation for enterprise apps</p>
+            <Link to="https://webinars.devops.com/how-to-build-an-ai-native-foundation-for-enterprise-apps">
+              Watch Video
             </Link>
           </div>
           <div className={styles.marketBox}>
+            <div className={styles.marketImage}></div>
+            <span>WEBINAR</span>
+            <p>
+              Beyond Vector Search: Taking a hybrid approach for better results
+            </p>
+            <Link to="https://events.weaviate.io/hybrid-search-webinar">
+              Watch Video
+            </Link>
+          </div>
+          <div className={styles.marketBox}>
+            <div className={styles.marketImage}></div>
+            <span>WEBINAR</span>
             <p>
               Multimodal Product Discovery: The next generation of search for
               ecommerce
             </p>
             <Link to="https://events.weaviate.io/ecommerce-webinar">
-              {'Register now >'}
-            </Link>
-          </div>
-          <div className={styles.marketBox}>
-            <p>How to build an ai-native foundation for enterprise apps</p>
-            <Link to="https://webinars.devops.com/how-to-build-an-ai-native-foundation-for-enterprise-apps">
-              {'Register now >'}
+              Watch Video
             </Link>
           </div>
         </div>
--- src/components/Events/Workshops/index.jsx
+++ src/components/Events/Workshops/index.jsx
@@ -24,15 +24,6 @@ export default function WorkshopSection() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.headerBox}>
-        <h2>Our upcoming workshops & events around the world</h2>
-        <p>
-          We believe that the next wave of software infrastructure is AI-first
-          and that a strong open-source community is a basis for creating
-          high-quality software. <br/>Our workshops deliver new information and
-          details on our service.
-        </p>
-      </div>
       <div className={styles.workshopWrapper}>
         <div className="workshopContainer">
           {!isCalendarLoaded && (
